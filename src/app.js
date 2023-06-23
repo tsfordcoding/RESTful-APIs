3bc75857969ea116ef8648b02c6a0c4fff408d7a
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-06-23 16:17:15 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const pastes = require("./data/pastes-data");
+
+// TODO: Follow instructions in the checkpoint to implement ths API.
+
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if(foundPaste) {
+    res.json({ data: foundPaste }); 
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
+// Not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.send(error);
+});
+
+module.exports = app;
